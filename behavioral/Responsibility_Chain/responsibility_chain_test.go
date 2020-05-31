1565e830f6091363e7db17a112c86a895d236cc2
Matthew-xx <1659530762@qq.com>
2020-05-31 11:13:18 +0800

    责任链模式

---
+++ behavioral/Responsibility_Chain/responsibility_chain_test.go
@@ -0,0 +1,15 @@
+package Responsibility_Chain
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHandler_Handle(t *testing.T) {
+	wang := NewHandler("laowang",nil,1)
+	zhang := NewHandler("laozhang",wang,2)
+	r := wang.Handler(1)
+	fmt.Println(r)
+	r = zhang.Handler(2)
+	fmt.Println(r)
+}
